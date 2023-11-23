b0e22aad02db753a381ce62b254dd5dc1b64ecb9
Jesús Cova <jesuscova@jisparking.com>
2023-11-23 17:39:16 -0300

    cambios

--- src/pages/Nationality.vue
+++ src/pages/Nationality.vue
@@ -24,7 +24,7 @@
 
         <div v-else class="flex flex-col pt-10">
             <h2 class="text-4xl dark:text-white pb-10">
-                Mantenedor Nacionalidades
+                Nacionalidades
                 <router-link
                     href="javascript:;"
                     to="/create_nationality"
