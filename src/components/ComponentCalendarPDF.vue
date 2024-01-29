64d9d53b38986f2dcee70d1fbac26a81eb4ad22a
ByronDein <byrondein@gmail.com>
2024-01-29 17:45:46 -0300

    ruts

--- src/components/ComponentCalendarPDF.vue
+++ src/components/ComponentCalendarPDF.vue
@@ -269,6 +269,11 @@
             <p class="w-40 mb-5 font-bold text-xl">Firma Trabajador</p>
             
         </div>
+        <div class="flex flex-row items-between justify-around -mt-3.5 ">
+            <p class="w-40 mb-5 font-bold text-xl">76063822-6</p>
+            <p class="w-40 mb-5 font-bold text-xl">{{ rutAndPeriodPDf.visual_rut }}</p>
+            
+        </div>
     </div>
     </div>
 </div>
